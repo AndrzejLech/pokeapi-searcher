e8a9c984dd8643b3940770b7051b9a34138bc25b
lcan <lcan@viessmann.com>
2022-05-30 05:54:02 +0200

    add retrofit

--- app/src/main/java/andrzej/lech/pokeapisearcher/network/RetrofitHandler.kt
+++ app/src/main/java/andrzej/lech/pokeapisearcher/network/RetrofitHandler.kt
@@ -12,20 +12,10 @@ class RetrofitHandler {
     private val TAG = "RetrofitHandler"
     private val retrofitInstance = RetrofitHelper.getInstance().create(RetrofitService::class.java)
 
-    fun getPokemonList(): Channel<ApiResponse> {
-        val channel: Channel<ApiResponse> = Channel()
-        CoroutineScope(Dispatchers.Main).launch {
+   suspend fun getPokemonList(): ApiResponse? {
             val result = retrofitInstance.getPokemonList()
             Log.d(TAG, result.body().toString())
-            result.body()?.results?.forEach {
-                it.name = it.name.replaceFirstChar { firstChar ->
-                    if (firstChar.isLowerCase())
-                        firstChar.titlecase(Locale.ROOT)
-                    else firstChar.toString()
-                }
-            }
-            result.body()?.let { channel.send(it) }
-        }
-        return channel
+
+            return result.body()
     }
 }
--- app/src/main/java/andrzej/lech/pokeapisearcher/network/RetrofitService.kt
+++ app/src/main/java/andrzej/lech/pokeapisearcher/network/RetrofitService.kt
@@ -1,11 +1,7 @@
 package andrzej.lech.pokeapisearcher.network
 
 import andrzej.lech.pokeapisearcher.network.models.ApiResponse
-import retrofit2.Call
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import retrofit2.http.GET
 
 interface RetrofitService {
